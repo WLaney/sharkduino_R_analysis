1e4ed33592bbbe384b129cb9d852c132d920a33d
William Laney <williamlaney@yahoo.com>
2017-02-10 14:09:44 -0500

    created intall script
    this script will install all dependent packages needed for this repo.
    please update with script with new depencies as they arise

---
+++ install.R
@@ -0,0 +1,11 @@
+#install dependent packages
+
+#I don't like the way R handles selcting mirrors on the mac so I am hard coding one in
+duke_repo<-"http://archive.linux.duke.edu/cran/" 
+
+#install packages
+install.packages("ggplot2", repos=duke_repo)
+install.packages("psd", repos=duke_repo)
+install.packages("signal", repos=duke_repo)
+install.packages("seewave", repos=duke_repo)
+install.packages("viridis", repos=duke_repo)
