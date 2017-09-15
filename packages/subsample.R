fa6a7549ec612c739af727529c5104547f4a0f3e
Dara Kharabi <kharabid@gmail.com>
2017-09-15 17:44:01 -0400

    fixed subsample errors introduced yesterday

--- packages/subsample.R
+++ packages/subsample.R
@@ -7,11 +7,14 @@
 
 # subsamples obj to 1/ssres points, taking the average of the source samples
 ss = function(obj, ssres=1) {
-  buckets = matrix(obj, ssres, floor(length(obj)/ssres))
+  if (ssres == 1) return(obj)
+  nCols = floor(length(obj)/ssres)
+  buckets = matrix(obj[1:(nCols*ssres)], ssres, nCols)
   return(colMeans(buckets))
 }
 
 # subsamples obj to 1/ssres points by returning every nth point
 ss.simple = function(obj, ssres=1) {
+  if (ssres == 1) return(obj)
   return(obj[seq(1, length(obj), ssres)])
 }
