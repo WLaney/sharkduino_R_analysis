55325129cc2dd357e1e42cdae9cf7285037c3353
hanqiu <hpeng@email.wm.edu>
2017-09-15 17:48:30 -0400

    simple change

--- simple_scatterplot.R
+++ simple_scatterplot.R
@@ -8,13 +8,13 @@ source("packages/subsample.R")
 
 # Read in raw CSV. Place your raw CSV file in the data subdirectory,
 # then edit the following line to reference it.
-data = import_data("../test.csv", legacy=F)
+data = import_data("data/backpack_data.csv", legacy=F)
 
 ## Simple scatterplot of one axis
 # Which points to plot?
-dataRange = 1:nrow(data)
+dataRange = 1:10000
 # subsampling resolution?
-ssres = 1000
+ssres = 1
 # dataset?
 ds = 3
 # plot with ggplot2
@@ -22,7 +22,7 @@ qplot(
   x = ss(data[[7]][dataRange],ssres), 
   y = ss(data[[ds]][dataRange],ssres), 
   geom = "line") + #scale_y_continuous(breaks = -12:12/10) + 
-  scale_x_datetime(breaks = date_breaks("10 sec"), labels = date_format("%S")) +
+  scale_x_datetime(breaks = date_breaks("1 min"), labels = date_format("%M")) +
   labs(
     x="Time", 
     y="Sensor Data", 
