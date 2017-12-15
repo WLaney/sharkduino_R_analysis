455f26ea0680b9091abd58eb3b03ca9db47f2741
hanqiu <hpeng@email.wm.edu>
2017-12-15 17:55:29 -0500

    script for windowed datasets

--- window_data.R
+++ window_data.R
@@ -1,10 +1,20 @@
 source("packages/import_data.R")
 library(ggplot2)
 require("cowplot")
-
-data = import_data("/Users/penghanqiu/Desktop/sharkduino_R_analysis/data/2017-8-22 7_51_51_21a _data.csv", legacy=F)
-length_window = 10
+require("scales")
+data = import_data("/Users/penghanqiu/Desktop/sharkduino_R_analysis/data/2017-9-1 6_24_39_22c _data.csv", legacy=F) #data for 11 minutes
+#data=data[453:3045]#from 02:25:00 to 02:27:00 #data for the first 2 minutes
+#length_window =25 #for 30 minutes
+length_window =10 #data for 2 minutes 
 n_windows= floor(nrow(data[,1])/length_window)
+
+#calculate the sample rate
+Time=data[,7]
+dif=Time[2:nrow(Time)]-Time[1:nrow(Time)-1]
+sample_rate=1/as.numeric(mean(dif[[1]]), units="secs")
+
+
+time=data[seq(1, nrow(data), length_window),7]
 
 separator=rep(1:n_windows, each=length_window)
 rest_rows=rep(n_windows+1,times=nrow(data)-length_window*n_windows)
@@ -30,43 +40,63 @@ rms_data=aggregate(data[,1:6], data.table(c(separator,rest_rows)), root_means_qu
 rms_data_with_window=cbind(rms_data,window_length)
 
 png('/Users/penghanqiu/Desktop/Rplot_mean//rplot_correlation.png', width = 2000, height = 2000, res = 200)
-plot(mean_data)
+plot(mean_data,col=rgb(red=0, green=0, blue=0, alpha=0.3),pch=20)
 dev.off()
+
+#png('/Users/penghanqiu/Desktop/Rplot_mean//rplot_ax_az.png', width = 2000, height = 2000, res = 200)
+#plot(mean_data[,c(1,3)],type="l")
+#dev.off()
+
+#plot(mean_data[5156:7733,c(1,3)],type="l",col="blue")
 
 # expression("Gyro X-Axis ("*degree*"/s)")
 #"acceleration X (Gs)"
-rplot_ax=qplot(1:nrow(mean_data_with_window), mean_data_with_window[,1], geom="line") + ylab("acceleration X (Gs)") + xlab("row number") 
+rplot_ax=qplot(time[[1]], mean_data_with_window[,1], geom="line") + 
+  ylab("acceleration X (Gs)") + xlab("time is hours:minutes:seconds")+
+  ggtitle(paste(length_window,"samples for each data point\n", sample_rate,"samples/second"))+scale_x_datetime(breaks = date_breaks("20 sec"), labels = date_format("%H:%M:%S"))
 ggsave(
   "/Users/penghanqiu/Desktop/Rplot_mean//rplot_ax.png",
   rplot_ax
 )
 
-rplot_ay=qplot(1:nrow(mean_data_with_window), mean_data_with_window[,2], geom="line") + ylab("acceleration Y (Gs)") + xlab("row number") 
+
+rplot_ay=qplot(time[[1]], mean_data_with_window[,2], geom="line") + ylab("acceleration Y (Gs)") + 
+  xlab("time is hours:minutes:seconds")+ ggtitle(paste(length_window,"samples for each data point\n", sample_rate,"samples/second"))+
+  scale_x_datetime(breaks = date_breaks("20 sec"), labels = date_format("%H:%M:%S"))
 ggsave(
   "/Users/penghanqiu/Desktop/Rplot_mean//rplot_ay.png",
   rplot_ay
 )
 
-rplot_az=qplot(1:nrow(mean_data_with_window), mean_data_with_window[,3], geom="line") + ylab("acceleration Z (Gs)") + xlab("row number") 
+rplot_az=qplot(time[[1]], mean_data_with_window[,3], geom="line") + 
+  ylab("acceleration Z (Gs)") + xlab("time is hours:minutes:seconds")+ggtitle(paste(length_window,"samples for each data point\n", sample_rate,"samples/second"))+
+  scale_x_datetime(breaks = date_breaks("20 sec"), labels = date_format("%H:%M:%S"))
 ggsave(
   "/Users/penghanqiu/Desktop/Rplot_mean//rplot_az.png",
   rplot_az
 )
 
-rplot_gx=qplot(1:nrow(mean_data_with_window), mean_data_with_window[,4], geom="line") + ylab(expression("Gyro X-Axis ("*degree*"/s)")) + xlab("row number") 
+rplot_gx=qplot(time[[1]], mean_data_with_window[,4], geom="line") + 
+  ylab(expression("Gyro X-Axis ("*degree*"/s)")) + xlab("time is hours:minutes:seconds")+ggtitle(paste(length_window,"samples for each data point\n", sample_rate,"samples/second"))+
+  scale_x_datetime(breaks = date_breaks("20 sec"), labels = date_format("%H:%M:%S"))
 ggsave(
   "/Users/penghanqiu/Desktop/Rplot_mean//rplot_gx.png",
   rplot_gx
 )
 
-rplot_gy=qplot(1:nrow(mean_data_with_window), mean_data_with_window[,5], geom="line") + ylab(expression("Gyro Y-Axis ("*degree*"/s)")) + xlab("row number") 
+rplot_gy=qplot(time[[1]], mean_data_with_window[,5], geom="line") + 
+  ylab(expression("Gyro Y-Axis ("*degree*"/s)")) + xlab("time is hours:minutes:seconds")+ggtitle(paste(length_window,"samples for each data point\n", sample_rate,"samples/second"))+
+  scale_x_datetime(breaks = date_breaks("20 sec"), labels = date_format("%H:%M:%S")) 
 ggsave(
   "/Users/penghanqiu/Desktop/Rplot_mean//rplot_gy.png",
   rplot_gy
 )
 
-rplot_gz=qplot(1:nrow(mean_data_with_window), mean_data_with_window[,6], geom="line") + ylab(expression("Gyro Z-Axis ("*degree*"/s)")) + xlab("row number") 
+rplot_gz=qplot(time[[1]], mean_data_with_window[,6], geom="line") + 
+  ylab(expression("Gyro Z-Axis ("*degree*"/s)")) + xlab("time is hours:minutes:seconds")+ggtitle(paste(length_window,"samples for each data point\n", sample_rate,"samples/second"))+
+  scale_x_datetime(breaks = date_breaks("20 sec"), labels = date_format("%H:%M:%S"))
 ggsave(
   "/Users/penghanqiu/Desktop/Rplot_mean//rplot_gz.png",
   rplot_gz
 )
+
